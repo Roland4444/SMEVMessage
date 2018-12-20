9fa3a5e235994d7e1eaef5dd3d593f17be9414a0
Roland4444 <turbopollux@rambler.ru>
2018-12-20 10:51:45 +0400

    Adding contructoir in ESIAFindMessage

--- src/main/java/Message/toSMEV/ESIAFindMessage.java
+++ src/main/java/Message/toSMEV/ESIAFindMessage.java
@@ -5,6 +5,21 @@ import Message.abstractions.BinaryMessage;
 import java.io.*;
 
 public class ESIAFindMessage implements BinaryMessage {
+
+    public ESIAFindMessage(String ID, String RA, String SNILSOper, String Surname, String FirstName, String MiddleName,
+                           String PassSeria, String PassNumber,  String Mobile, String SNiLS){
+        this.ID=ID;
+        this.Ra=RA;
+        this.OperatorSnils=ESIAFindMessage.getSNILSfromplain(SNILSOper);
+        this.Surname=Surname;
+        this.Name=FirstName;
+        this.MiddleName=MiddleName;
+        this.Passnumber=PassNumber;
+        this.Passseria=PassSeria;
+        this.MobileNumber=ESIAFindMessage.getMobilefromplain(Mobile);
+        this.SNILS=SNiLS;
+
+    }
     public String Name;
     public String Surname;
     public String MiddleName;
