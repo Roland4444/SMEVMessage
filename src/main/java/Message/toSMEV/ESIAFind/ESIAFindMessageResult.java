2d84feb6ff593d3f0f4c44478430d6b77347a157
Roland4444 <turbopollux@rambler.ru>
2018-12-20 13:50:28 +0400

    TestPassed

--- src/main/java/Message/toSMEV/ESIAFind/ESIAFindMessageResult.java
+++ src/main/java/Message/toSMEV/ESIAFind/ESIAFindMessageResult.java
@@ -3,6 +3,7 @@ package Message.toSMEV.ESIAFind;
 import Message.abstractions.BinaryMessage;
 
 public class ESIAFindMessageResult implements BinaryMessage {
+    public String ID;
     public String oid;
     public String trusted;
 }
