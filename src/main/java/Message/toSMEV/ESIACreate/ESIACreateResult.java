213f613b3e3f12505bf6220f87a1856998c2a671
Roland4444 <turbopollux@rambler.ru>
2018-12-25 11:53:13 +0400

    extend CREATEESIAMessage

--- src/main/java/Message/toSMEV/ESIACreate/ESIACreateResult.java
+++
@@ -1,6 +0,0 @@
-package Message.toSMEV.ESIACreate;
-
-import Message.abstractions.BinaryMessage;
-
-public class ESIACreateResult implements BinaryMessage {
-}
---
+++ src/main/java/Message/toSMEV/ESIAUpgrade/ESIAUpgradeInitial.java
@@ -0,0 +1,8 @@
+package Message.toSMEV.ESIAUpgrade;
+
+
+import Message.toSMEV.ESIACreate.ESIACreateInit;
+
+public class ESIAUpgradeInitial  extends ESIACreateInit {
+    public String OID;
+}
