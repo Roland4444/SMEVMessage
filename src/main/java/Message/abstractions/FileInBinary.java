eef99f4efdab8266ccc1d71d86915ddc564f9f2a
Roland4444 <turbopollux@rambler.ru>
2019-01-25 12:35:16 +0400

    adding to File suspend and restore

--- src/main/java/Message/abstractions/FileInBinary.java
+++ src/main/java/Message/abstractions/FileInBinary.java
@@ -1,6 +1,23 @@
 package Message.abstractions;
 
+import Message.toSMEV.EBS.Essens.PhotoBundle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileInBinary implements BinaryMessage {
     public String filename;
     public byte[] fileContent;
+    public static void suspendToDisk(PhotoBundle pb) throws IOException {
+        FileOutputStream fos = new FileOutputStream(pb.filename);
+        fos.write(pb.fileContent);
+        fos.close();
+    }
+
+    public static void clean(PhotoBundle pb) throws IOException {
+        File f =  new File(pb.filename);
+        if (f.exists())
+            f.delete();
+    }
 }
--- src/main/java/Message/toSMEV/EBS/Essens/PhotoBundle.java
+++ src/main/java/Message/toSMEV/EBS/Essens/PhotoBundle.java
@@ -7,15 +7,5 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class PhotoBundle extends FileInBinary {
-    public static void suspendToDisk(PhotoBundle pb) throws IOException {
-        FileOutputStream fos = new FileOutputStream(pb.filename);
-        fos.write(pb.fileContent);
-        fos.close();
-    }
 
-    public static void clean(PhotoBundle pb) throws IOException {
-       File f =  new File(pb.filename);
-       if (f.exists())
-            f.delete();
-    }
 }
