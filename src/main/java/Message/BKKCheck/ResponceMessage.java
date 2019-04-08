82aff16ee0426d72e9cb05725a08da9fafc15652
Roland4444 <turbopollux@rambler.ru>
2019-04-08 14:09:28 +0400

    adding additional fileds

--- src/main/java/Message/BKKCheck/ResponceMessage.java
+++ src/main/java/Message/BKKCheck/ResponceMessage.java
@@ -5,12 +5,15 @@ import Message.abstractions.BinaryMessage;
 import java.io.*;
 
 public class ResponceMessage implements BinaryMessage {
+    public int checkResult;
+    public String ID;
+    public String ProblemName;
+    public Double problemValue;
     public ResponceMessage(int checkResult, String ID){
         this.checkResult=checkResult;
         this.ID=ID;
     }
-    public int checkResult;
-    public String ID;
+
 
 
 
